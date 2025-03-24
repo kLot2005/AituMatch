c8680100935cab6ad12e78799ea3b9a8796778f9
kLot2005 <bekars.bihimbai2005@gmail.com>
2025-03-24 21:03:13 +0500

    done

---
+++ src/main/java/com/ka15er/AituMatchApplication.java
@@ -0,0 +1,12 @@
+package com.ka15er;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AituMatchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AituMatchApplication.class, args);
+    }
+}
